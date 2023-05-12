2cbb09c1743221f95aaaa26763dadd58e3538a4f
Federico Pinna <fpinna@vivocha.com>
2023-05-12 12:38:58 +0200

    fix: improve types for the rules compiler

---
+++ src/rules.test.ts
@@ -0,0 +1,78 @@
+import { expect } from 'chai';
+import { Capabilities, Rule, Tags, compileRules } from './rules.js';
+
+describe('compileRules', () => {
+  it('should compile rules', () => {
+    const source = `#
+  drop
+    not ethertype ipv4
+    and not ethertype arp
+    and not ethertype ipv6
+  ;
+  
+  tag server
+    id 2
+    enum 0 No
+    enum 1 Yes
+    default No;
+  
+  drop
+    not tor server 1
+  ;
+  
+  accept;
+  `;
+    const rules: Rule[] = [];
+    const capabilities: Capabilities = {};
+    const tags: Tags = {};
+
+    compileRules(source, rules, capabilities, tags);
+    expect(rules).to.deep.equal([
+      {
+        etherType: 2048,
+        not: true,
+        or: false,
+        type: 'MATCH_ETHERTYPE',
+      },
+      {
+        etherType: 2054,
+        not: true,
+        or: false,
+        type: 'MATCH_ETHERTYPE',
+      },
+      {
+        etherType: 34525,
+        not: true,
+        or: false,
+        type: 'MATCH_ETHERTYPE',
+      },
+      {
+        type: 'ACTION_DROP',
+      },
+      {
+        id: 2,
+        not: true,
+        or: false,
+        type: 'MATCH_TAGS_BITWISE_OR',
+        value: 1,
+      },
+      {
+        type: 'ACTION_DROP',
+      },
+      {
+        type: 'ACTION_ACCEPT',
+      },
+    ]);
+    expect(tags).to.deep.equal({
+      server: {
+        default: 0,
+        enums: {
+          no: 0,
+          yes: 1,
+        },
+        flags: {},
+        id: 2,
+      },
+    });
+  });
+});
--- types/zerotier-rule-compiler.d.ts
+++ types/zerotier-rule-compiler.d.ts
@@ -1,4 +1,3 @@
 declare module 'zerotier-rule-compiler/rule-compiler.js' {
-  import { ZeroTier } from '@covisian/zerotier.js';
-  export function compile(src: string, rules: ZeroTier.Controller.Network['rules'], caps: ZeroTier.Controller.Network['capabilities'], tags: ZeroTier.Controller.Network['tags']): any;
+  export function compile(src: string, rules: any[], caps: any, tags: any): any;
 }
